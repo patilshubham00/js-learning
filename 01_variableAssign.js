d31aab63986a63401e5e19430c1357887a0f8711
patilshubham00 <patilshubham.0x@gmail.com>
2024-03-08 11:30:15 +0530

    Variable Assignment 01
    Variable Assignment 01

---
+++ 01_variableAssign.js
@@ -0,0 +1,42 @@
+
+
+console.log("======== Fll Name ==========");
+
+var myName;
+myName = "Shubham Patil";
+console.log(myName);
+
+var myFullName;
+myFullName="Shubham Satish Patil";
+console.log(myFullName);
+
+
+console.log("======== Love ==========");
+
+var myLove;
+myLove ="JS";
+console.log("I Love Only",myLove);
+
+
+console.log("======== Info ==========");
+var firstName;
+var lastName;
+firstName="Shubham"
+lastName="Patil"
+console.log(firstName);
+console.log(lastName);
+var myLove;
+myLove ="JS";
+console.log("I Love Only",myLove);
+var city;
+city="Bamani"
+console.log(city);
+var pinCode;
+pinCode="413317"
+console.log(pinCode);
+var gender;
+gender="Male"
+console.log(gender);
+var married;
+married="Unmarried"
+console.log(married);
