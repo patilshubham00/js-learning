5fa696fc5e847a33831ee8d83e4d7a0dea366c63
patilshubham00 <patilshubham.0x@gmail.com>
2024-03-12 07:04:54 +0530

    Function Assignment 02
    Function Assignment 02

---
+++ 03_funAssign02.js
@@ -0,0 +1,15 @@
+
+
+function bankDetails(bankName,accountNum,location,pinCode) {
+    console.log("My Bank Name Is:",bankName);
+    console.log("My Account Number Is:",accountNum); 
+    console.log("My Bank Location Is:",location);
+    console.log("My Bank Pincode Is:",pinCode);  
+   }
+   bankDetails("SBI Bank",2674563221,'Sangola',413317);
+   console.log("*");
+   
+   bankDetails("Axis Bank",7856782345,'Pune',441202);
+   console.log("*");
+   
+   bankDetails("HDFC Bank",7585682345,'Solapur',631202,);
