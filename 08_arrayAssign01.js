6d378518fcd7e87e6400bf6a5165ad32e6c4d590
patilshubham00 <patilshubham.0x@gmail.com>
2024-03-21 10:13:02 +0530

    08 Fruit Seasonal Assignment 01
    08 Fruit Seasonal Assignment 01

---
+++ 08_arrayAssign01.js
@@ -0,0 +1,69 @@
+
+
+var fruits_seasonal = ["Banana" , "Orange" , "Apple" , "Mango" , "Water Melon"];
+console.log("==================== Original array ====================");
+console.log(fruits_seasonal);
+
+console.log("==================== Step 1 ====================");
+
+console.log(fruits_seasonal.length-1);
+
+console.log("==================== Step 2 ====================");
+
+var fruits_seasonal = ["Banana" , "Orange" , "Apple" , "Mango" , "Water Melon"];
+console.log(fruits_seasonal);
+fruits_seasonal.unshift("papaya");
+console.log(fruits_seasonal);
+
+console.log("==================== Step 3 ====================");
+
+var fruits_seasonal = ["Banana" , "Orange" , "Apple" , "Mango" , "Water Melon"];
+console.log(fruits_seasonal);
+const deleteElement = fruits_seasonal.splice(3, 1);
+console.log(deleteElement);
+console.log(fruits_seasonal);
+
+
+console.log("==================== Step 4 ====================");
+
+var fruits_seasonal = ["Banana" , "Orange" , "Apple" , "Mango" , "Water Melon"];
+console.log(fruits_seasonal);
+fruits_seasonal.push("Pineapple");
+console.log(fruits_seasonal);
+
+
+
+console.log("==================== Step 5 ====================");
+
+var fruits_seasonal = ["Banana" , "Orange" , "Apple" , "Mango" , "Water Melon"];
+console.log(fruits_seasonal);
+const removeElement = fruits_seasonal.splice(4, 0, "Dragon Fruit");
+console.log(removeElement);
+console.log(fruits_seasonal);
+
+
+
+console.log("==================== Step 6 ====================");
+
+var fruits_seasonal = ["Banana" , "Orange" , "Apple" , "Mango" , "Water Melon"];
+console.log(fruits_seasonal);
+const deleted = fruits_seasonal.splice(1, 1, "Kiwi");
+console.log(deleted);
+console.log(fruits_seasonal);
+
+
+console.log("==================== Step 7 ====================");
+
+var fruits_seasonal = ["Banana" , "Orange" , "Apple" , "Mango" , "Water Melon"];
+console.log(fruits_seasonal);
+const subArray =fruits_seasonal.slice(1, 5);
+console.log(subArray);
+
+
+
+console.log("==================== Step 7 ====================");
+
+var fruits_seasonal = ["Banana" , "Orange" , "Apple" , "Mango" , "Water Melon"];
+console.log(fruits_seasonal);
+const result = fruits_seasonal.slice(fruits_seasonal.length-3);
+console.log(result);
