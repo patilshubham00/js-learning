b5a37747ac5ddb9b5bcb3e3ef78a519e8f4e9b8b
patilshubham00 <patilshubham.0x@gmail.com>
2024-04-04 17:27:31 +0530

    JSON Assignment
    JSON Assignment

--- 10_jsonAssign.js
+++ 10_jsonAssign.js
@@ -1,7 +1,7 @@
 
 
 let person = {
-"name" :["Aleix " , " Melon"], 
+"name" :["Aleix Melon"], 
 "id" : "E00245",
 "role" : ["Dev", "DBA"], 
 "age" : 23 ,
@@ -42,16 +42,25 @@ console.log("========================= Convert this one to object ==============
 const personObject = JSON.parse(personJSON);
 console.log(typeof personObject);
 console.log(personObject);
+console.table(personObject);
 
 
 console.log("========================= Log on console role ---> 'Dev' =========================");
 
-console.log(person.role[0]);
+console.log(`first Element of role is =${person.role[0]}`);
 
 console.log("========================= Log only last name  ---> 'Melon' =========================");
 
-console.log(person.name[1]);
+var array = ["Aleix" , "Melon"];
+console.log(array);
+const result = array.splice(1);
+console.log(`Last Element of name is = ${result}`);
 
 
 
 console.log("========================= Log only joining year of employee  ---> 2019 =========================");
+
+var array = ["11","12","2019"];
+console.log(array);
+const newArray  = array.splice(2);
+console.log(`Joining Year of employee is = ${newArray}`);
