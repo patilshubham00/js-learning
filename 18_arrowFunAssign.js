94cfd64da661b8761b0a6caef9b9bb8baa720a7d
patilshubham00 <patilshubham.0x@gmail.com>
2024-04-09 07:56:43 +0530

    Arrow Function Assignment
    Arrow Function Assignment

---
+++ 18_arrowFunAssign.js
@@ -0,0 +1,40 @@
+
+
+console.log(`==================== Step 1 ====================`);
+
+
+
+let show = () => {
+    console.log("Good Morning , Today Is Monday ");
+}
+show();
+
+
+
+console.log(`==================== Step 2 ====================`);
+
+
+
+function multiplication(n1, n2 , n3=1){
+    console.log(`n1:${n1} , n2:${n2} , n3:${n3}`);
+    console.log(`Multiplication is ${n1*n2*n3}`);
+}
+multiplication(5, 5, 2);
+multiplication(10, 4,)
+
+
+
+console.log(`==================== Step 3 ====================`);
+
+
+
+let add = (n1, n2, n3 ,n4, n5) => {
+    console.log(`Addition is : ${n1+n2+n3+n4+n5}`);
+}
+add(100, 100, 200, 349, 756);
+add("I Am " , " learning " , " ES6 " , " features " , " in Depth");
+
+
+
+
+
